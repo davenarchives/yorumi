e16b75381965563885d2afdacb196da44463dd92
davenarchives <daventheabyss@gmail.com>
2026-01-13 04:23:28 +0800

    types: update anime types

--- src/types/anime.ts
+++ src/types/anime.ts
@@ -26,21 +26,31 @@ export interface Anime {
     duration?: string;
     rating?: string;
     season?: string;
-    trailer?: {
-        youtube_id?: string;
-        url?: string;
-        embed_url?: string;
-        images?: {
-            image_url?: string;
-            small_image_url?: string;
-            medium_image_url?: string;
-            large_image_url?: string;
-            maximum_image_url?: string;
-        };
-    };
+
     anilist_banner_image?: string;
     anilist_cover_image?: string;
     latestEpisode?: number; // For ongoing anime - the latest aired episode
+    characters?: {
+        edges: {
+            role: string;
+            node: {
+                id: number;
+                name: { full: string };
+                image: { large: string };
+            };
+            voiceActors: {
+                id: number;
+                name: { full: string };
+                image: { large: string };
+                languageV2: string;
+            }[];
+        }[];
+    };
+    trailer?: {
+        id: string; // Youtube ID
+        site: string;
+        thumbnail: string;
+    };
 }
 
 export interface Episode {
